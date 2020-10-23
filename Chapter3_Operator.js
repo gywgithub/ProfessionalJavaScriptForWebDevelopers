c2f5b784f4d243a39346f39ec1525d157ab84d66
gywgithub <qingyi_w@outlook.com>
2020-10-23 17:25:08 +0800

    add and or

--- Chapter3_Operator.js
+++ Chapter3_Operator.js
@@ -67,3 +67,99 @@ console.log(o); // -1
 
 s1 = -s1;
 console.log(s1); // -1
+
+let num11 = 25;
+let num21 = ~num11;
+console.log(num21); // -26
+
+let num12 = 25;
+let num22 = -num12 - 1;
+console.log(num22); // -26
+
+let num13 = -25;
+let num23 = ~num13;
+console.log(num23); // 24
+
+let result = 25 & 3;
+console.log(Number(25).toString(2)); // 二进制数值：11001
+console.log(Number(3).toString(2)); // 二进制数值： 11
+console.log(result); // 1
+
+let result2 = 25 | 3;
+console.log(Number(25).toString(2)); // 二进制:11001
+console.log(Number(3).toString(2)); // 二进制：11
+console.log(result2); // 十进制： 27
+
+let result3 = 25 ^ 3;
+console.log(result3); // 26
+
+let oldValue = 2;
+let newValue = oldValue << 5;
+console.log(oldValue); // 2
+console.log(newValue); // 64
+
+console.log('---');
+
+let oldValue2 = 64;
+let newValue2 = oldValue2 >> 5;
+console.log(oldValue2); // 64
+console.log(newValue2); // 2
+
+console.log('###');
+
+let oldValue3 = 2;
+let newValue3 = oldValue3 >> 5;
+console.log(oldValue3); // 2
+console.log(newValue3); // 0
+
+console.log('$$$');
+
+let oldValue4 = 64;
+let newValue4 = oldValue4 >>> 5;
+console.log(newValue4); // 2
+
+let oldValue5 = -64;
+let newValue5 = oldValue5 >>> 5;
+console.log(newValue5); // 134327726
+
+console.log('%%%');
+
+console.log(!false); // true
+console.log(!'blue'); // false
+console.log(!0); // true
+console.log(!NaN); // true
+console.log(!''); // true
+console.log(!12345); // false
+
+console.log('^^^');
+
+console.log(!!'blue'); // true
+console.log(!!0); // false
+console.log(!!NaN); // false
+console.log(!!''); // false
+console.log(!!123); // true
+console.log(Boolean(124));
+if (123) {
+  console.log(1);
+}
+
+let found = true;
+// let result5 = (found && someUndeclaredVariable); // ReferenceError: someUndeclaredVariable is not defined.    // 短路特性
+// console.log(result5);
+
+let found2 = false;
+let result6 = (found2 && someUndeclaredVariable);
+console.log(result6); // false
+
+let found3 = false;
+// let result7 = (found3 || someUndeclaredVariable); // ReferenceError: someUndeclaredVariable is not defined.      // 短路特性
+// console.log(result7);
+
+let found4 = true;
+let result8 = (found4 || someUndeclaredVariable);
+console.log(result8); // true
+
+let preferredObject = null;
+let backupObject = 'abc';
+let myObject = preferredObject || backupObject;
+console.log(myObject); 
