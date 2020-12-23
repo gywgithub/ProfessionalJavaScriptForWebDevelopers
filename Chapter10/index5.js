484f35e28cdcab3a4e1fee4bb5c742bc028f1ece
gywgithub <qingyi_w@outlook.com>
2020-12-23 20:52:14 +0800

    add index5.js

---
+++ Chapter10/index5.js
@@ -0,0 +1,27 @@
+function createComparisonFunction(propertyName) {
+  return function(object1, object2) {
+    let value1 = object1[propertyName];
+    let value2 = object2[propertyName];
+    if (value1 < value2) {
+      return -1;
+    } else if (value1 > value2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+function compare(value1, value2) {
+  if (value1 < value2) {
+    return -1;
+  } else if (value1 > value2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+let result = compare(5, 10);
+console.log(result);
+
