9e0a145083aa8561598daa697403e5f76579ea92
gywgithub <qingyi_w@outlook.com>
2021-07-31 15:05:36 +0800

    update main.js

--- Chapter27/main.js
+++ Chapter27/main.js
@@ -8,3 +8,24 @@ const worker = new Worker('./emptyWorker.js');
 console.log(worker);
 
 // globalScopeWorker.js
+
+// service worker
+navigator.serviceWorker.register('/serviceWorker.js')
+  .then((serviceWorkerRegistration) => {
+    console.log(serviceWorkerRegistration)
+  })
+
+console.log(caches)
+
+const request1 = new Request('https://www.foo.com')
+const response1 = new Response('fooResponse')
+
+caches.open('v1')
+  .then((cache) => {
+    cache.put(request1, response1)
+      .then(() => cache.keys())
+      .then(console.log)
+      .then(() => cache.delete(request1))
+      .then(() => cache.keys())
+      .then(console.log)
+  })
